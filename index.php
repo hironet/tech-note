03b4e0035c70ae639082d56b0d488cda3ae07e81
Hiro <hiroyuki1126@gmail.com>
2021-03-21 13:26:51 +0900

    ディレクトリ構造を変更。

--- index.php
+++ index.php
@@ -1,34 +1,5 @@
 <?php
-include_once(__DIR__ . '/common/functions.php');
-include_once(__DIR__ . '/common/routing.php');
+include_once(__DIR__ . '/controllers/routing.php');
+include_once(__DIR__ . '/views/functions.php');
+include_once(__DIR__ . '/views/main.php');
 ?>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-<?php include_once(__DIR__ . '/common/head.php'); ?>
-</head>
-<body>
-  <header>
-<?php include_once(__DIR__ . '/common/navbar.php'); ?>
-  </header>
-  <div class="container">
-    <div class="row">
-      <div class="col-12 col-lg-9">
-        <main>
-<?php include_once(__DIR__ . $contents_path); ?>
-        </main>
-      </div><!-- col -->
-      <div class="col-12 col-lg-3">
-        <aside>
-<?php include_once(__DIR__ . '/common/sidebar.php'); ?>
-        </aside>
-      </div><!-- col -->
-    </div><!-- row -->
-  </div><!-- container -->
-  <hr class="featurette-divider">
-  <footer>
-<?php include_once(__DIR__ . '/common/footer.php'); ?>
-  </footer>
-<?php include_once(__DIR__ . '/common/script.php'); ?>
-</body>
-</html>
---
+++ views/main.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<?php include_once(__DIR__ . '/head.php'); ?>
+</head>
+<body>
+  <header>
+<?php include_once(__DIR__ . '/navbar.php'); ?>
+  </header>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-lg-9">
+        <main>
+<?php include_once(__DIR__ . $contents_path); ?>
+        </main>
+      </div><!-- col -->
+      <div class="col-12 col-lg-3">
+        <aside>
+<?php include_once(__DIR__ . '/sidebar.php'); ?>
+        </aside>
+      </div><!-- col -->
+    </div><!-- row -->
+  </div><!-- container -->
+  <hr class="featurette-divider">
+  <footer>
+<?php include_once(__DIR__ . '/footer.php'); ?>
+  </footer>
+<?php include_once(__DIR__ . '/script.php'); ?>
+</body>
+</html>

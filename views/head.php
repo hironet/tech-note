7fa203ff5ba39763f409097ed0898cfc93ec252f
Hiro <hiroyuki1126@gmail.com>
2021-03-21 15:29:11 +0900

    メタ情報を追加。

--- views/head.php
+++ views/head.php
@@ -1,5 +1,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="ITエンジニアHiroの技術メモです。">
+<meta name="keywords" content="ITエンジニア,Hiro,技術メモ,tech note">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" tegrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 <title><?= empty($contents_title) ? '' : $contents_title . ' | ' ?>Hiro's tech note</title>
 <style>

f1365405c1dc439168814db94f502d42c5153859
Hiro <hiroyuki1126@gmail.com>
2021-03-22 08:36:23 +0900

    軽微な修正。

--- views/analyticstracking.php
+++ views/analyticstracking.php
@@ -1,11 +1,11 @@
 <?php if (strcmp($_SERVER['HTTP_HOST'], 'hironet.org') === 0) { ?>
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-F904Q49SRS"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-F904Q49SRS"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-    gtag('config', 'G-F904Q49SRS');
-  </script>
+  gtag('config', 'G-F904Q49SRS');
+</script>
 <?php } ?>

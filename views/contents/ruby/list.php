3797915f62bd5525d3e52a7c389d194232e4347d
Hiro <hiroyuki1126@gmail.com>
2021-03-23 16:00:52 +0900

    変数名、関数名を変更。

--- views/contents/ruby/list.php
+++ views/contents/ruby/list.php
@@ -1,6 +1,6 @@
 <?php
-$pub_date = '2021-03-22 00:00:00';
-$mod_date = get_datetime_modified(__FILE__);
+$ptime = '2021-03-22 00:00:00';
+$mtime = get_mtime(__FILE__);
 ?>
 <header>
 <?php include_once(__DIR__ . '/../../common/header.php'); ?>

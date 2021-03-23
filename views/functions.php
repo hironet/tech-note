0822e949a93406c21fe49175ce8ff1e5e8ab030b
Hiro <hiroyuki1126@gmail.com>
2021-03-23 16:00:00 +0900

    関数名、引数名を変更。

--- views/functions.php
+++ views/functions.php
@@ -1,14 +1,14 @@
 <?php
-function get_datetime_modified($file) {
+function get_mtime($file) {
   return date('Y-m-d H:m:s', filemtime($file));
 }
 
-function get_datetime_iso8601($datetime) {
-  $dt = new DateTime($datetime);
+function get_time_iso8601($time) {
+  $dt = new DateTime($time);
   return $dt->format(DateTime::ATOM);
 }
 
-function get_date_japanese($datetime) {
-  $dt = new DateTime($datetime);
+function get_date_jp($time) {
+  $dt = new DateTime($time);
   return $dt->format('Y年m月d日');
 }

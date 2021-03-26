b822f8d33e63ead9e2168744f92d153f3e337c51
Hiro <hiroyuki1126@gmail.com>
2021-03-26 23:27:19 +0900

    パンくずリストを削除。

--- views/common/header.php
+++ views/common/header.php
@@ -21,15 +21,3 @@ foreach ($breadcrumb_list as $breadcrumb => $url) {
   </ol>
 </nav>
 <h1><?= $contents_title ?></h1>
-<ul>
-  <li>公開日：
-    <time itemprop="datePublished" datetime="<?= get_time_iso8601($ptime) ?>">
-      <?= get_date_jp($ptime), PHP_EOL ?>
-    </time>
-  </li>
-  <li>更新日：
-    <time itemprop="dateModified" datetime="<?= get_time_iso8601($mtime) ?>">
-      <?= get_date_jp($mtime), PHP_EOL ?>
-    </time>
-  </li>
-</ul>

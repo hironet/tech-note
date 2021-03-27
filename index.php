3bcc90de30c0da6e7d055b9280a56ab4bc93a17a
Hiro <hiroyuki1126@gmail.com>
2021-03-27 15:02:29 +0900

    軽微な修正。

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-$meta_description = '';
-$meta_keywords = '';
 $contents_title = '';
 $contents_path = '';
+$meta_description = '';
+$meta_keywords = '';
 $breadcrumb_list = [];
 
 include_once(__DIR__ . '/controllers/routing.php');

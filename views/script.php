cf5b0b9c4917a6e6575020413b39043eaa1b06a1
Hiro <hiroyuki1126@gmail.com>
2021-03-27 23:34:35 +0900

    code-prettifyの読み込み処理を追加。

--- views/script.php
+++ views/script.php
@@ -1,4 +1,5 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/run_prettify.js?skin=desert"></script>
 <?php if (strcmp($_SERVER['HTTP_HOST'], 'hironet.org') === 0) { ?>
 <script src="https://hironet.org/portfolio/common.js"></script>
 <?php } else { ?>

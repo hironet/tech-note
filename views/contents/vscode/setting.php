4a0dd2f8dbce758a9f03f7f97f1764fb7bc7810e
Hiro <hiroyuki1126@gmail.com>
2021-04-03 22:34:14 +0900

    内容を追加。

--- views/contents/vscode/setting.php
+++ views/contents/vscode/setting.php
@@ -6,3 +6,12 @@ $mtime = get_mtime(__FILE__);
 <?php include_once(__DIR__ . '/../common/header.php'); ?>
 <?php include_once(__DIR__ . '/../common/date.php'); ?>
 </header>
+<h2 class="title">基本設定</h2>
+<p>1. コマンドパレットを開く。</p>
+<p>2. <code>settings.json</code>と入力する。</p>
+<p>3. <code>基本設定: 設定 (JSON) を開く</code>を選択し、<kbd>Enter</kbd>を押す。</p>
+<h2 class="title">プログラミング言語固有の設定</h2>
+<p>1. コマンドパレットを開く。</p>
+<p>2. <code>Preferences: Configure Language Specific Settings</code>と入力する。</p>
+<p>3. <code>基本設定: 言語固有の設定を構成します</code>を選択し、<kbd>Enter</kbd>を押す。</p>
+<p>4. 設定したいプログラミング言語を選択し、<kbd>Enter</kbd>を押す。</p>

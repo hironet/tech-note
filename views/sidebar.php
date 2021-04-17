a91d9299b8f9f4032484313855be3014423c4b92
Hiro <hiroyuki1126@gmail.com>
2021-04-17 22:54:12 +0900

    自己紹介を追加。

--- views/sidebar.php
+++ views/sidebar.php
@@ -1,1 +1,7 @@
-<h2>よく読まれている記事</h2>
+<div class="profile-box">
+  <img class="rounded mx-auto d-block" src="/tech-note/img/profile.webp" alt="Hiro" width="80" height="80">
+  <div class="text-center mb-2">
+    <span class="fw-bold">ITエンジニア Hiro</span>
+  </div>
+  <p>このサイトは、ITエンジニアHiroの技術メモです。私が今までITについて調査したり、実践してきたことの中で、有益だと思った情報をまとめていますので、ぜひ参考にしてください。私に関する情報は、<a href="/portfolio/">こちら</a>をご覧ください。</p>
+</div>

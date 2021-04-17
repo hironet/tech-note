2d6514492162be08caed75af3e5a80d614d7a999
Hiro <hiroyuki1126@gmail.com>
2021-04-17 19:04:06 +0900

    軽微な修正。

--- views/contents/wordpress/performance.php
+++ views/contents/wordpress/performance.php
@@ -6,13 +6,13 @@ $mtime = get_mtime(__FILE__);
 <?php include_once(__DIR__ . '/../common/header.php'); ?>
 <?php include_once(__DIR__ . '/../common/date.php'); ?>
 </header>
-<h2 class="title">リビジョン削除</h2>
+<h3 class="title">リビジョン削除</h3>
 <p>データベース（MySQL）に接続し、以下SQLを実行する。</p>
 <pre class="block"><code>USE wordpress;
 SELECT COUNT(*) FROM wp_posts WHERE post_type='revision';
 DELETE FROM wp_posts WHERE post_type='revision';
 SELECT COUNT(*) FROM wp_posts WHERE post_type='revision';</code></pre>
-<h2 class="title">wp-postsテーブルのフラグメント化解消</h2>
+<h3 class="title">wp-postsテーブルのフラグメント化解消</h3>
 <p>データベース（MySQL）に接続し、リビジョンを削除した後、以下SQLを実行する。</p>
 <pre class="block"><code>USE wordpress;
 SELECT @@sql_mode;

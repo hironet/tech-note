8cf08ef009e2b5427b7327085d7e1246adc5023c
Hiro <hiroyuki1126@gmail.com>
2021-05-04 20:12:06 +0900

    内容を追加。

--- views/contents/php/array.php
+++ views/contents/php/array.php
@@ -12,7 +12,7 @@ $mtime = get_mtime(__FILE__);
     <a href="#1dime-array">1次元配列の基本操作</a>
   </li>
   <li>
-    <a href="#string">配列と文字列</a>
+    <a href="#string">配列の文字列への展開</a>
   </li>
   <li>
     <a href="#convenient">配列の便利な操作</a>
@@ -21,46 +21,75 @@ $mtime = get_mtime(__FILE__);
 <h2 id="1dime-array" class="title">1次元配列の基本操作</h2>
 <h3 class="title">配列の作成</h3>
 <p>array()で空の配列を作成する。</p>
-<pre class="block"><code>$array = array();</code></pre>
+<pre class="block"><code class="php">$colors = array();</code></pre>
 <p>短縮構文で空の配列を作成する。</p>
-<pre class="block"><code>$array = [];</code></pre>
+<pre class="block"><code class="php">$colors = [];</code></pre>
 <h3 class="title">添字配列の初期化</h3>
 <p>array()で添字配列を初期化する。</p>
-<pre class="block"><code>$array = array('red', 'green', 'blue');
-echo $array[0];  # red</code></pre>
+<pre class="block"><code class="php">$colors = array('red', 'green', 'blue');
+echo $colors[0];  # red</code></pre>
 <p>短縮構文で添字配列を初期化する。</p>
-<pre class="block"><code>$array = ['red', 'green', 'blue'];
-echo $array[2];  # blue</code></pre>
+<pre class="block"><code class="php">$colors = ['red', 'green', 'blue'];
+echo $colors[2];  # blue</code></pre>
 <h3 class="title">連想配列の初期化</h3>
 <p>array()で連想配列を初期化する。</p>
-<pre class="block"><code>$array = array('product_no' => 10, 'product_name' => 'iPhone 12', 'price' => 94380);
-echo $array['product_name'];  # iPhone 12</code></pre>
+<pre class="block"><code class="php">$product = array('product_no' => 10, 'product_name' => 'iPhone 12', 'price' => 94380);
+echo $product['product_name'];  # iPhone 12</code></pre>
 <p>短縮構文で連想配列を初期化する。</p>
-<pre class="block"><code>$array = ['product_no' => 10, 'product_name' => 'iPhone 12', 'price' => 94380];
-echo $array['price'];  # 94380</code></pre>
-<h2 id="string" class="title">配列と文字列</h2>
+<pre class="block"><code class="php">$product = ['product_no' => 10, 'product_name' => 'iPhone 12', 'price' => 94380];
+echo $product['price'];  # 94380</code></pre>
+<h2 id="string" class="title">配列の文字列への展開</h2>
 <p>1次元配列の値を文字列内に展開する。</p>
 <ul>
   <li>配列を<code>{}</code>で囲む必要はない。</li>
   <li>キーを<code>'</code>（シングルクォーテーション）で囲んではならない。</li>
 </ul>
-<pre class="block"><code>$array = ['product_no' => 10, 'product_name' => 'iPhone 12', 'price' => 94380];
-echo "製品名は{$array[product_name]}です。";  # 製品名はiPhone12です。
-echo "製品名は$array[product_name]です。";  # 製品名はiPhone12です。</code></pre>
+<pre class="block"><code class="php">$product = ['product_no' => 10, 'product_name' => 'iPhone 12', 'price' => 94380];
+echo "製品名は{$product[product_name]}です。";  # 製品名はiPhone12です。
+echo "製品名は$product[product_name]です。";  # 製品名はiPhone12です。</code></pre>
 <p>多次元配列の値を文字列内に展開する。</p>
 <ul>
   <li>必ず配列を<code>{}</code>で囲む必要がある。</li>
   <li>キーを<code>'</code>（シングルクォーテーション）で囲んではならない。</li>
 </ul>
-<pre class="block"><code>$array = [
+<pre class="block"><code class="php">$products = [
   ['product_no' => 10, 'product_name' => 'iPhone 12', 'price' => 94380],
   ['product_no' => 11, 'product_name' => 'iPhone 12 Pro', 'price' => 117480],
   ['product_no' => 12, 'product_name' => 'iPhone 12 Pro Max', 'price' => 129580]
 ];
-echo "製品名は{$array[1][product_name]}です。";  # 製品名はiPhone12 Proです。</code></pre>
+echo "製品名は{$products[1][product_name]}です。";  # 製品名はiPhone12 Proです。</code></pre>
 <h2 id="convenient" class="title">配列の便利な操作</h2>
-<h3 class="title">list構文</h3>
+<h3 class="title">配列の分解（list構文）</h3>
 <p>配列から複数の変数に代入する。</p>
-<pre class="block"><code>$color_array = ['red', 'green', 'blue'];
-list($color_1, $color_2, $color_3) = $color_array;
-echo "${$color_1}, ${$color_2}, ${$color_3}";  # red, green, blue</code></pre>
+<pre class="block"><code class="php">$colors = ['red', 'green', 'blue'];
+list($color_1, $color_2, $color_3) = $colors;
+echo "{$color_1}, {$color_2}, {$color_3}";  # red, green, blue</code></pre>
+<p>配列の1番目と2番目の要素のみ変数に代入する。</p>
+<pre class="block"><code class="php">$colors = ['red', 'green', 'blue'];
+list($color_1, $color_2) = $colors;
+echo "{$color_1}, {$color_2}";  # red, green</code></pre>
+<p>配列の1番目と3番目の要素のみ変数に代入する。</p>
+<pre class="block"><code class="php">$colors = ['red', 'green', 'blue'];
+list($color_1, , $color_3) = $colors;
+echo "{$color_1}, {$color_3}";  # red, blue</code></pre>
+<p>配列の3番目の要素のみ変数に代入する。</p>
+<pre class="block"><code class="php">$colors = ['red', 'green', 'blue'];
+list(, , $color_3) = $colors;
+echo "{$color_3}";  # blue</code></pre>
+<h3 class="title">配列の分解（短縮構文）</h3>
+<p>配列から複数の変数に代入する。</p>
+<pre class="block"><code class="php">$colors = ['red', 'green', 'blue'];
+[$color_1, $color_2, $color_3] = $colors;
+echo "{$color_1}, {$color_2}, {$color_3}";  # red, green, blue</code></pre>
+<p>配列の1番目と2番目の要素のみ変数に代入する。</p>
+<pre class="block"><code class="php">$colors = ['red', 'green', 'blue'];
+[$color_1, $color_2] = $colors;
+echo "{$color_1}, {$color_2}";  # red, green</code></pre>
+<p>配列の1番目と3番目の要素のみ変数に代入する。</p>
+<pre class="block"><code class="php">$colors = ['red', 'green', 'blue'];
+[$color_1, , $color_3] = $colors;
+echo "{$color_1}, {$color_3}";  # red, blue</code></pre>
+<p>配列の3番目の要素のみ変数に代入する。</p>
+<pre class="block"><code class="php">$colors = ['red', 'green', 'blue'];
+[, , $color_3] = $colors;
+echo "{$color_3}";  # blue</code></pre>

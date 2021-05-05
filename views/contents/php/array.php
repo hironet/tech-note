bd33ff7023ea6bd55ecf8050926e38b342b8ead7
Hiro <hiroyuki1126@gmail.com>
2021-05-05 13:26:39 +0900

    内容を追加・修正。

--- views/contents/php/array.php
+++ views/contents/php/array.php
@@ -12,6 +12,9 @@ $mtime = get_mtime(__FILE__);
     <a href="#1dime-array">1次元配列の基本操作</a>
   </li>
   <li>
+    <a href="#2dime-array">2次元配列の基本操作</a>
+  </li>
+  <li>
     <a href="#string">配列の文字列への展開</a>
   </li>
   <li>
@@ -19,32 +22,65 @@ $mtime = get_mtime(__FILE__);
   </li>
 </ul>
 <h2 id="1dime-array" class="title">1次元配列の基本操作</h2>
-<h3 class="title">空の配列の作成</h3>
+<h3 class="title">空の1次元配列の作成</h3>
 <p>array()で空の配列を作成する。</p>
 <pre class="block"><code class="php">$colors = array();</code></pre>
 <p>短縮構文で空の配列を作成する。</p>
 <pre class="block"><code class="php">$colors = [];</code></pre>
-<h3 class="title">配列の作成・初期化（添字配列の場合）</h3>
+<h3 class="title">1次元配列の作成・初期化（添字配列の場合）</h3>
 <p>array()で添字配列を作成・初期化する。</p>
 <pre class="block"><code class="php">$colors = array('red', 'green', 'blue');
 echo $colors[0];  # red</code></pre>
 <p>短縮構文で添字配列を作成・初期化する。</p>
 <pre class="block"><code class="php">$colors = ['red', 'green', 'blue'];
 echo $colors[2];  # blue</code></pre>
-<h3 class="title">配列の作成・初期化（連想配列の場合）</h3>
+<h3 class="title">1次元配列の作成・初期化（連想配列の場合）</h3>
 <p>array()で連想配列を作成・初期化する。</p>
-<pre class="block"><code class="php">$product = array('product_no' => 10, 'product_name' => 'iPhone 12', 'price' => 94380);
+<pre class="block"><code class="php">$product = array('product_no' => 11, 'product_name' => 'iPhone 12', 'price' => 94380);
 echo $product['product_name'];  # iPhone 12</code></pre>
 <p>短縮構文で連想配列を作成・初期化する。</p>
-<pre class="block"><code class="php">$product = ['product_no' => 10, 'product_name' => 'iPhone 12', 'price' => 94380];
+<pre class="block"><code class="php">$product = ['product_no' => 11, 'product_name' => 'iPhone 12', 'price' => 94380];
 echo $product['price'];  # 94380</code></pre>
+<h2 id="2dime-array" class="title">2次元配列の基本操作</h2>
+<h3 class="title">2次元配列の作成・初期化（添字配列の場合）</h3>
+<p>array()で添字配列を作成・初期化する。</p>
+<pre class="block"><code class="php">$colors = array(
+  array('red', 'green', 'blue'),
+  array('yellow', 'cyan', 'magenta'),
+  array('black', 'white')
+);
+echo $colors[0][0];  # red</code></pre>
+<p>短縮構文で添字配列を作成・初期化する。</p>
+<pre class="block"><code class="php">$colors = [
+  ['red', 'green', 'blue'],
+  ['yellow', 'cyan', 'magenta'],
+  ['black', 'white']
+];
+echo $colors[2][1];  # white</code></pre>
+<h3 class="title">2次元配列の作成・初期化（連想配列の場合）</h3>
+<p>array()で連想配列を作成・初期化する。</p>
+<pre class="block"><code class="php">$products = array(
+  array('product_no' => 11, 'product_name' => 'iPhone 12', 'price' => 94380),
+  array('product_no' => 12, 'product_name' => 'iPhone 12 Pro', 'price' => 117480),
+  array('product_no' => 13, 'product_name' => 'iPhone 12 Pro Max', 'price' => 129580)
+);
+echo $products[1]['product_name'];  # iPhone 12 Pro
+</code></pre>
+<p>短縮構文で連想配列を作成・初期化する。</p>
+<pre class="block"><code class="php">$products = [
+  ['product_no' => 11, 'product_name' => 'iPhone 12', 'price' => 94380],
+  ['product_no' => 12, 'product_name' => 'iPhone 12 Pro', 'price' => 117480],
+  ['product_no' => 13, 'product_name' => 'iPhone 12 Pro Max', 'price' => 129580]
+];
+echo $products[2]['price'];  # 129580
+</code></pre>
 <h2 id="string" class="title">配列の文字列への展開</h2>
 <p>1次元配列の値を文字列内に展開する。</p>
 <ul>
   <li>配列を<code>{}</code>で囲む必要はない。</li>
   <li>キーを<code>'</code>（シングルクォーテーション）で囲んではならない。</li>
 </ul>
-<pre class="block"><code class="php">$product = ['product_no' => 10, 'product_name' => 'iPhone 12', 'price' => 94380];
+<pre class="block"><code class="php">$product = ['product_no' => 11, 'product_name' => 'iPhone 12', 'price' => 94380];
 echo "製品名は{$product[product_name]}です。";  # 製品名はiPhone12です。
 echo "製品名は$product[product_name]です。";  # 製品名はiPhone12です。</code></pre>
 <p>多次元配列の値を文字列内に展開する。</p>
@@ -53,9 +89,9 @@ echo "製品名は$product[product_name]です。";  # 製品名はiPhone12で�
   <li>キーを<code>'</code>（シングルクォーテーション）で囲んではならない。</li>
 </ul>
 <pre class="block"><code class="php">$products = [
-  ['product_no' => 10, 'product_name' => 'iPhone 12', 'price' => 94380],
-  ['product_no' => 11, 'product_name' => 'iPhone 12 Pro', 'price' => 117480],
-  ['product_no' => 12, 'product_name' => 'iPhone 12 Pro Max', 'price' => 129580]
+  ['product_no' => 11, 'product_name' => 'iPhone 12', 'price' => 94380],
+  ['product_no' => 12, 'product_name' => 'iPhone 12 Pro', 'price' => 117480],
+  ['product_no' => 13, 'product_name' => 'iPhone 12 Pro Max', 'price' => 129580]
 ];
 echo "製品名は{$products[1][product_name]}です。";  # 製品名はiPhone12 Proです。</code></pre>
 <h2 id="convenient" class="title">配列の便利な操作</h2>

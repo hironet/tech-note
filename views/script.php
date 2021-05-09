d80db721d33aeb5e5f02fea42060ed0c44fd6bb0
Hiro <hiroyuki1126@gmail.com>
2021-05-09 10:53:27 +0900

    common.jsのURLを変更。

--- views/script.php
+++ views/script.php
@@ -2,4 +2,4 @@
 <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.7.1/styles/hybrid.min.css">
 <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.7.1/highlight.min.js"></script>
 <script>hljs.initHighlightingOnLoad();</script>
-<script src="https://engineer-hiro.com/portfolio/common.js"></script>
+<script src="https://engineer-hiro.com/common.js"></script>

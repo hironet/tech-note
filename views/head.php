ce8b9d4a17a23a603774147946a2ebf39b7d6764
Hiro <hiroyuki1126@gmail.com>
2021-05-11 20:53:32 +0900

    bootstrap5.0.0にバージョンアップ。

--- views/head.php
+++ views/head.php
@@ -1,7 +1,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="このサイトは、ITエンジニアHiroの技術メモです。<?= $contents_desc ?>">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" tegrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
 <title><?= empty($contents_title) ? '' : $contents_title . ' | ' ?><?= SITE_NAME ?></title>
 <style>
   p, ul, ol, li {

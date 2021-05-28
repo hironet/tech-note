2a100bd9cff204acd81c68399c040f32b8db56a7
Hiro <hiroyuki1126@gmail.com>
2021-05-28 20:31:11 +0900

    旧Hiro's tech noteのファイルを削除。

--- views/footer.php
+++
@@ -1,3 +0,0 @@
-<div class="container text-center my-3">
-  <small>&copy; <?= date('Y') ?> ITエンジニア Hiro</small>
-</div>
--- views/script.php
+++
@@ -1,5 +0,0 @@
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.7.1/styles/hybrid.min.css">
-<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.7.1/highlight.min.js"></script>
-<script>hljs.initHighlightingOnLoad();</script>
-<script src="https://engineer-hiro.com/common.js"></script>

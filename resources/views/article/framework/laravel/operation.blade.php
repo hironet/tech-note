f014654e25184598a9aa706e0b1d4eb713ee427d
Hiro <hiroyuki1126@gmail.com>
2021-05-30 22:20:46 +0900

    コマンドを追加。

--- resources/views/article/framework/laravel/operation.blade.php
+++ resources/views/article/framework/laravel/operation.blade.php
@@ -20,6 +20,16 @@ $description = 'Laravelの基本的な操作方法について書いています
 $ composer create-project laravel/laravel [プロジェクト名] --prefer-dist
 $ chown -R www-data:www-data [プロジェクト名]/bootstrap/cache/
 $ chown -R www-data:www-data [プロジェクト名]/storage/</code></pre>
+<p>プロジェクトのGitリポジトリをクローンして作成する。</p>
+<ul>
+  <li><code>www-data</code>は、Apacheの実行ユーザ・グループ。</li>
+</ul>
+<pre class="block"><code class="shell">$ git clone [GitリポジトリのURL]
+$ cd [Gitリポジトリ]
+$ compose install
+$ chown -R www-data:www-data vendor</code></pre>
+<h3 class="title">ルート一覧確認</h3>
+<pre class="block"><code class="shell">$ php artisan route:list</code></pre>
 <h3 class="title">コントローラ作成</h3>
 <p>コントローラの雛形ファイルを作成する。</p>
 <pre class="block"><code class="shell">$ cd [プロジェクト名]

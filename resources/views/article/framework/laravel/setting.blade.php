32e14f1e56e950862c1d45490288edcdc9677f04
Hiro <hiroyuki1126@gmail.com>
2021-06-06 14:47:19 +0900

    プロジェクトの初期設定を追加。

--- resources/views/article/framework/laravel/setting.blade.php
+++ resources/views/article/framework/laravel/setting.blade.php
@@ -8,6 +8,41 @@ $description = 'Laravelの設定方法について書いています。';
 @section('contents')
 @include('components.header')
 <h2 class="title">設定方法</h2>
+<h3 class="title">プロジェクトの初期設定</h3>
+<h4><code>config/app.php</code></h4>
+<h5>修正前</h5>
+<pre class="block"><code class="apache">&hellip;
+    'name' => env('APP_NAME', 'Laravel'),
+&hellip;
+    'timezone' => 'UTC',
+&hellip;
+    'locale' => 'en',
+&hellip;
+</code></pre>
+<h5>修正後</h5>
+<pre class="block"><code class="apache">&hellip;
+    'name' => env('APP_NAME', '[アプリケーション名]'),
+&hellip;
+    'timezone' => 'Asia/Tokyo',
+&hellip;
+    'locale' => 'ja',
+&hellip;
+</code></pre>
+<h4><code>.env</code></h4>
+<h5>修正前</h5>
+<pre class="block"><code class="apache">&hellip;
+APP_NAME=Laravel
+&hellip;
+APP_URL=http://localhost
+&hellip;
+</code></pre>
+<h5>修正後</h5>
+<pre class="block"><code class="apache">&hellip;
+APP_NAME=[アプリケーション名]
+&hellip;
+APP_URL=[URL]
+&hellip;
+</code></pre>
 <h3 class="title">URLからpublicを除外する方法</h3>
 <p>プロジェクトのURL（<code>https://[ドメイン]/[プロジェクト名]/public</code>）から<code>public</code>を除いたURLでトップページにアクセスできるようにするには、以下の<code>.htaccess</code>ファイルをプロジェクトのトップディレクトリ直下に配置すればよい。</p>
 <pre class="block"><code class="apache">&lt;IfModule mod_rewrite.c&gt;

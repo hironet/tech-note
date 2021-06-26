3293dbeec74e7f23514b932b45b0418214f85864
Hiro <hiroyuki1126@gmail.com>
2021-06-26 23:04:32 +0900

    新規作成。

---
+++ resources/views/article/middleware/mysql/database.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts/app')
+
+@section('contents')
+<header>
+@include('components.header')
+  <nav>
+    <ul class="toc">
+      <li>
+        <a href="#create">データベース作成</a>
+      </li>
+      <li>
+        <a href="#drop">データベース削除</a>
+      </li>
+    </ul>
+  </nav>
+</header>
+<h2 id="create" class="title">データベース作成</h2>
+<p>1. 作成するデータベースが存在しないことを確認する。</p>
+<pre class="block"><code class="sql">SHOW DATABASES;</code></pre>
+<p>2. データベースを作成する。</p>
+<pre class="block"><code class="sql">CREATE DATABASE [データベース名] DEFAULT CHARACTER SET [文字コード] COLLATE [照合順序];</code></pre>
+<p>3. 作成したデータベースが存在することを確認する。</p>
+<pre class="block"><code class="sql">SHOW DATABASES;</code></pre>
+<h2 id="drop" class="title">データベース削除</h2>
+<p>1. 削除するデータベースが存在することを確認する。</p>
+<pre class="block"><code class="sql">SHOW DATABASES;</code></pre>
+<p>2. データベースを削除する。</p>
+<pre class="block"><code class="sql">DROP DATABASE [データベース名];</code></pre>
+<p>3. 削除したデータベースが存在しないことを確認する。</p>
+<pre class="block"><code class="sql">SHOW DATABASES;</code></pre>
+@endsection

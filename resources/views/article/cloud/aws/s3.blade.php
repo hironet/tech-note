4c613b3b122fee395f9f02abda8aecd7be5d91b5
Hiro <hiroyuki1126@gmail.com>
2021-06-27 16:39:20 +0900

    内容を修正。

--- resources/views/article/cloud/aws/s3.blade.php
+++ resources/views/article/cloud/aws/s3.blade.php
@@ -10,72 +10,32 @@
   </nav>
 </header>
 <h2 id="command" class="title">S3のコマンド</h2>
-<div class="table-responsive">
-  <table class="table table-striped table-sm">
-    <thead>
-      <tr>
-        <th>操作</th>
-        <th>コマンド</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>全てのバケットを一覧表示する。</td>
-        <td><pre class="inline"><code class="shell">$ aws s3 ls</code></pre></td>
-      </tr>
-      <tr>
-        <td>バケット内の全てのオブジェクトを一覧表示する。</td>
-        <td><pre class="inline"><code class="shell">$ aws s3 ls s3://[バケット名]</code></pre></td>
-      </tr>
-      <tr>
-        <td>東京リージョンにバケットを作成する。</td>
-        <td><pre class="inline"><code class="shell">$ aws s3 mb s3://[バケット名] --region ap-northeast-1</code></pre></td>
-      </tr>
-      <tr>
-        <td>バケットを削除する。</td>
-        <td><pre class="inline"><code class="shell">$ aws s3 rb s3://[バケット名]</code></pre></td>
-      </tr>
-      <tr>
-        <td>バケット内の全てのオブジェクトを削除し、バケットを削除する。</td>
-        <td><pre class="inline"><code class="shell">$ aws s3 rb s3://[バケット名] --force</code></pre></td>
-      </tr>
-      <tr>
-        <td>オブジェクトを削除する。</td>
-        <td><pre class="inline"><code class="shell">$ aws s3 rm s3://[バケット名]/[ファイル名]</code></pre></td>
-      </tr>
-      <tr>
-        <td>ローカルのファイルをバケットに移動する。</td>
-        <td><pre class="inline"><code class="shell">$ aws s3 mv [ファイル名] s3://[バケット名]</code></pre></td>
-      </tr>
-      <tr>
-        <td>バケット内のオブジェクトをローカルのカレントディレクトリに移動する。</td>
-        <td><pre class="inline"><code class="shell">$ aws s3 mv s3://[バケット名]/[ファイル名] .</code></pre></td>
-      </tr>
-      <tr>
-        <td>ローカルのファイルをバケットにコピーする。</td>
-        <td><pre class="inline"><code class="shell">$ aws s3 cp [ファイル名] s3://[バケット名]</code></pre></td>
-      </tr>
-      <tr>
-        <td>バケット内のオブジェクトをローカルのカレントディレクトリにコピーする。</td>
-        <td><pre class="inline"><code class="shell">$ aws s3 cp s3://[バケット名]/[ファイル名] .</code></pre></td>
-      </tr>
-      <tr>
-        <td>バケット内をカレントディレクトリと同期する。</td>
-        <td><pre class="inline"><code class="shell">$ aws s3 sync . s3://[バケット名]</code></pre></td>
-      </tr>
-      <tr>
-        <td>バケット内をカレントディレクトリと同期する。<br>カレントディレクトリに存在しないファイルはバケット内から削除する。</td>
-        <td><pre class="inline"><code class="shell">$ aws s3 sync . s3://[バケット名] --delete</code></pre></td>
-      </tr>
-      <tr>
-        <td>カレントディレクトリをバケット内と同期する。</td>
-        <td><pre class="inline"><code class="shell">$ aws s3 sync s3://[バケット名] .</code></pre></td>
-      </tr>
-      <tr>
-        <td>カレントディレクトリをバケット内と同期する。<br>バケット内に存在しないファイルはカレントディレクトリから削除する。</td>
-        <td><pre class="inline"><code class="shell">$ aws s3 sync s3://[バケット名] . --delete</code></pre></td>
-      </tr>
-    </tbody>
-  </table>
-</div><!-- /.table-responsive -->
+<h3 class="oper">全てのバケットを一覧表示する</h3>
+<pre class="block"><code class="shell">$ aws s3 ls</code></pre>
+<h3 class="oper">バケット内の全てのオブジェクトを一覧表示する</h3>
+<pre class="block"><code class="shell">$ aws s3 ls s3://[バケット名]</code></pre>
+<h3 class="oper">東京リージョンにバケットを作成する</h3>
+<pre class="block"><code class="shell">$ aws s3 mb s3://[バケット名] --region ap-northeast-1</code></pre>
+<h3 class="oper">バケットを削除する</h3>
+<pre class="block"><code class="shell">$ aws s3 rb s3://[バケット名]</code></pre>
+<h3 class="oper">バケット内の全てのオブジェクトを削除し、バケットを削除する</h3>
+<pre class="block"><code class="shell">$ aws s3 rb s3://[バケット名] --force</code></pre>
+<h3 class="oper">オブジェクトを削除する</h3>
+<pre class="block"><code class="shell">$ aws s3 rm s3://[バケット名]/[ファイル名]</code></pre>
+<h3 class="oper">ローカルのファイルをバケットに移動する</h3>
+<pre class="block"><code class="shell">$ aws s3 mv [ファイル名] s3://[バケット名]</code></pre>
+<h3 class="oper">バケット内のオブジェクトをローカルのカレントディレクトリに移動する</h3>
+<pre class="block"><code class="shell">$ aws s3 mv s3://[バケット名]/[ファイル名] .</code></pre>
+<h3 class="oper">ローカルのファイルをバケットにコピーする</h3>
+<pre class="block"><code class="shell">$ aws s3 cp [ファイル名] s3://[バケット名]</code></pre>
+<h3 class="oper">バケット内のオブジェクトをローカルのカレントディレクトリにコピーする</h3>
+<pre class="block"><code class="shell">$ aws s3 cp s3://[バケット名]/[ファイル名] .</code></pre>
+<h3 class="oper">バケット内をカレントディレクトリと同期する</h3>
+<pre class="block"><code class="shell">$ aws s3 sync . s3://[バケット名]</code></pre>
+<h3 class="oper">バケット内をカレントディレクトリと同期する<br>（カレントディレクトリに存在しないファイルはバケット内から削除する）</h3>
+<pre class="block"><code class="shell">$ aws s3 sync . s3://[バケット名] --delete</code></pre>
+<h3 class="oper">カレントディレクトリをバケット内と同期する</h3>
+<pre class="block"><code class="shell">$ aws s3 sync s3://[バケット名] .</code></pre>
+<h3 class="oper">カレントディレクトリをバケット内と同期する<br>（バケット内に存在しないファイルはカレントディレクトリから削除する）</h3>
+<pre class="block"><code class="shell">$ aws s3 sync s3://[バケット名] . --delete</code></pre>
 @endsection

7a75f568e6fa14b9ef701782fcefa8f7c747de06
Hiro <hiroyuki1126@gmail.com>
2021-06-27 17:58:20 +0900

    tech-note.cssを削除。

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,6 @@
 @endempty
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
   <link href="{{ secure_asset('/css/common.css') }}" rel="stylesheet">
-  <link href="{{ secure_asset('/css/tech-note.css') }}" rel="stylesheet">
 @empty ($article_title)
   <title>{{ config('article.SITE_NAME') }}</title>
 @else
